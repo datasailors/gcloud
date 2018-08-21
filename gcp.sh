438bde1934feb2c4c1d46eb054823fb0bb05847d
Alex Iankoulski <iankouls@ge.com>
2018-08-21 16:48:25 -0700

    Generalize DOCKER_REGISTRY setting

--- gcp.sh
+++ gcp.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-docker container run -it --rm  -e http_proxy=$http_proxy -e https_proxy=$https_proxy -e no_proxy=$no_proxy -v $(pwd):/wd -v ~/.boto:/root/.boto -v ~/.config:/root/.config  ${DOCKER_REGISTRY}/gcp gcloud $@
+docker container run -it --rm  -e http_proxy=$http_proxy -e https_proxy=$https_proxy -e no_proxy=$no_proxy -v $(pwd):/wd -v ~/.boto:/root/.boto -v ~/.config:/root/.config  ${DOCKER_REGISTRY}gcp gcloud $@
 
